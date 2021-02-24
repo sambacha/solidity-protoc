690ab53488608eb445376a92242a364b197564cd
sam <sam@freighttrust.com>
2021-02-24 11:17:38 -0800

    update

--- test/generate_sol_pb.sh
+++ test/generate_sol_pb.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
+echo "==> Removing old solidity and protobuf files..."
 # remove old sol and pb files
 rm -f *.pb
 rm -f solidity/contracts/lib/*.sol
 
 # generate new sol files
 export PATH="$TRAVIS_BUILD_DIR:$PATH"
-protoc --sol_out=importpb=true:solidity/contracts/lib/ test.proto a.proto b.proto
+protoc --sol_out=importpb=true:solidity/contracts/lib/ test.proto a.proto b.proto 
 
+echo "==> Generating protobuf files..."
 # generate new pb files
 for pathname in *.textpb; do
     filename=${pathname##*/}
@@ -15,7 +16,8 @@ for pathname in *.textpb; do
     msg=${basename%%_*}
     msgno=${msg#msg}
     cat "$basename".textpb|protoc  --encode=mytest.Msg"$msgno" test.proto > "$basename".pb || continue
-done
+done || exit 1
 
 # generate binary msg for b to test import
 protoc --encode=b.B b.proto < b.pbtxt > b.pb
+echo "==> Generation Complete"
